e34986d52ec92ba2cd9056b8550b64eb2ec20e88
Kevin Ge <gekevin2000@gmail.com>
2020-07-02 13:21:09 -0400

    import fast IK

---
+++ JuggleBot/Assets/Scripts/TargetScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(move(transform.position + 5f*Vector3.up));
+        }
+    }
+
+    // Update is called once per frame
+    IEnumerator move(Vector3 destination)
+    {
+        while (transform.position != destination)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, 15f * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
