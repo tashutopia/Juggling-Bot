efcc787e1948f2a51fe2d5b9892c690ff395382c
apazhetam <techx.paz@gmail.com>
2020-07-12 19:59:49 -0400

    Revert "Revert "Revert "Moved the ThrowThree.cs component from Ball1 to Brain"""
    This reverts commit 8178fda4110bd2b1578b593dbc6991b0aa5316dd.

--- JuggleBot/Assets/Scripts/ThrowThree.cs
+++ JuggleBot/Assets/Scripts/ThrowThree.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,28 +8,25 @@ public class ThrowThree : MonoBehaviour
     public Vector3 ThrowingPower_right = new Vector3();
     public float time;
     public float timeBetweenThrows;
-    public GameObject Ball_One;
     public GameObject Ball_Two;
     public GameObject Ball_Three;
     public GameObject Target_Left;
     public GameObject Target_Right;
-    public Rigidbody rb_one;
+    private Rigidbody rb;
     public Rigidbody rb_two;
     public Rigidbody rb_three;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb_one = Ball_One.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         rb_two = Ball_Two.GetComponent<Rigidbody>();
         rb_three = Ball_Three.GetComponent<Rigidbody>();
-
-        rb_one.useGravity = false;
+        rb.useGravity = false;
         rb_two.useGravity = false;
         rb_three.useGravity = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -45,35 +42,38 @@ public class ThrowThree : MonoBehaviour
 
     IEnumerator Throw_One()
     {
-        rb_one.useGravity = true;
-        rb_one.AddForce(ThrowingPower_left, ForceMode.Impulse);
+        rb.useGravity = true;
+        rb.AddForce(ThrowingPower_left,ForceMode.Impulse);
         yield return new WaitForSeconds(time);
         //Throws the purple ball towards the right hand and waits for 0.5 seconds
         rb_two.useGravity = true;
-        rb_two.AddForce(ThrowingPower_right, ForceMode.Impulse);
+        rb_two.AddForce(ThrowingPower_right,ForceMode.Impulse);
         yield return new WaitForSeconds(time*3/2);
         //Throws the green ball towards the left hand and waits for 0.75 seconds
         rb_three.useGravity = true;
-        rb_three.AddForce(ThrowingPower_left, ForceMode.Impulse);
+        rb_three.AddForce(ThrowingPower_left,ForceMode.Impulse);
         //Throws the purple ball towards the left hand
         yield return new WaitForSeconds(timeBetweenThrows);
         StartCoroutine(Throw_Two());
     }
 
+
     IEnumerator Throw_Two()
     {
-        rb_one.useGravity = true;
-        rb_one.AddForce(ThrowingPower_right, ForceMode.Impulse);
+        rb.useGravity = true;
+        rb.AddForce(ThrowingPower_right,ForceMode.Impulse);
         yield return new WaitForSeconds(time);
         //Throws the purple ball towards the right hand and waits for 0.5 seconds
         rb_two.useGravity = true;
-        rb_two.AddForce(ThrowingPower_left, ForceMode.Impulse);
+        rb_two.AddForce(ThrowingPower_left,ForceMode.Impulse);
         yield return new WaitForSeconds(time*3/2);
         //Throws the green ball towards the left hand and waits for 0.75 seconds
         rb_three.useGravity = true;
-        rb_three.AddForce(ThrowingPower_right, ForceMode.Impulse);
+        rb_three.AddForce(ThrowingPower_right,ForceMode.Impulse);
         //Throws the purple ball towards the left hand
         yield return new WaitForSeconds(timeBetweenThrows);
         StartCoroutine(Throw_One());
     }
+
+
 }
